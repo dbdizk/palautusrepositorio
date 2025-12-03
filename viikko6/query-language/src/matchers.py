19bd59fe549f64bd768b9f71f013c97b21e7c822
dbdizk <onninikku@gmail.com>
2025-12-03 13:15:28 +0200

    Teht3 done

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -27,3 +27,27 @@ class HasAtLeast:
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+
+class All:
+    def __init__(self):
+        self._player = self
+
+    def test(self,player):
+        return player
+    
+class Not:
+    def __init__(self, clause):
+        self._clause = clause
+    
+    def test(self,player):
+        return not bool(self._clause.test(player))
+    
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def test(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value < self._value
